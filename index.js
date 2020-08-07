d0b4f5f965e01f27a702b0a984a1ff23b3e894b0
Kayode <adeniyikayode22@gmail.com>
2020-08-07 22:55:20 +0100

    added eslint to enforce code style

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express= require('express')
-const app= express()
+const express = require('express')
+const app = express()
 
-app.get('/', function(req, res){
-    return res.send('IDUO Woman E-Commerce Website in progress');
+app.get('/', function (req, res) {
+  return res.send('IDUO Woman E-Commerce Website in progress')
 })
 
-app.listen(3000, function(){
-                console.log("Server running at port 3000");
+app.listen(3000, function () {
+  console.log('Server running at port 3000')
 })
