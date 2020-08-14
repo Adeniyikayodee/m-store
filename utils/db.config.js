8bc2bc7fa88db47b4067a39cedb772c3aee694c3
Kayode <adeniyikayode22@gmail.com>
2020-08-14 21:53:31 +0100

    added resgitration page and flash message

--- utils/db.config.js
+++ utils/db.config.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/iduowoman', {
+mongoose.connect('mongodb://localhost:27017/jamesbond', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
