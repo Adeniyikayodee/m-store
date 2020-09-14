07803b66ab574f15997d3dd2970de77d33a17b32
Kayode Adeniyi <adeniyikayode22@gmail.com>
2020-09-15 00:54:52 +0100

    refactored routes and categories

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const express = require('express')
 const session = require('express-session')
-const bodyParser = require('body-parser')
 const logger = require('morgan')
 require('./utils/db.config')
 const MongoStore = require('connect-mongo')(session)
@@ -11,10 +10,15 @@ require('./utils/authStrategies/localStrategy')
 const authMiddleware = require('./middlewares/authMiddleware')
 const flasherMiddleware = require('./middlewares/flasherMiddleware')
 const authRoutes = require('./routes/authRoutes')
+const categoryRoutes = require('./routes/categoryRoutes')
+const productRoutes = require('./routes/productRoutes')
+const categoryApiRoutes = require('./routes/api/categoryRoutes')
 const app = express()
 const config = require('./utils/config')
+const { trimAndSantizeObject, cutter } = require('./utils/global')
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 app.set('view engine', 'pug')
 // app.set('trust proxy', 1)
 app.use(session({
@@ -28,34 +32,45 @@ app.use(express.static('public'))
 app.use(logger('dev'))
 app.use(passport.initialize())
 app.use(passport.session())
+app.use((req, res, next) => {
+  trimAndSantizeObject(req.body)
+  return next()
+})
 
 /**
  * Global middleware to make logged in user available to the views
  */
 app.use((req, res, next) => {
+  res.locals.assetUrl = config.assetUrl
   res.locals.user = req.isAuthenticated() ? req.user : null
+  res.locals.cutter = cutter
   return next()
 })
 
 /**
  * App level locals
  */
+app.locals.title = 'X Store'
 app.locals.message = {} // Used in displaying alert
 app.locals.formData = {} // For prefilling data on form validation
 app.locals.errors = {} // Form validation errors
 
 app.use('/', authRoutes)
+app.use('/', categoryRoutes)
+app.use('/', productRoutes)
+app.use('/api/v1/category', categoryApiRoutes)
+app.use('/api/v1/category', categoryApiRoutes)
 
 app.get('/', flasherMiddleware, (req, res) => {
-  return res.render('index')
+  return res.render('pages/homepage')
 })
 
-app.get('/homepage', authMiddleware, (req, res) => {
-  return res.render('dashboard')
+app.get('/dashboard', authMiddleware, (req, res) => {
+  return res.render('dashboard/dashboard')
 })
 
 app.use((req, res, next) => {
-  res.status(404).render('404')
+  res.status(404).render('pages/404')
 })
 
 app.listen(config.port, () => {
@@ -63,3 +78,4 @@ app.listen(config.port, () => {
 })
 
 module.exports = app
+
